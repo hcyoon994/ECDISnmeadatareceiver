a68fabf35194e3be3c11c755c28c9756713ddad2
243_SMS <pt@marineworks.co.kr>
2025-04-24 11:14:09 +0900

    소스 정리

--- ECDISnmeadatareceiver/Form1.cs
+++ ECDISnmeadatareceiver/Form1.cs
@@ -1,16 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 using System.Text.Json;
-using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ECDISnmeadatareceiver
 {
